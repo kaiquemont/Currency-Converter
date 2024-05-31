f4eebbd22fae61d77a8068afca77b4753af12872
kaiquemont <kaique-monteiro67@hotmail.com>
2024-05-31 16:39:23 -0300

    primeiro conversor

---
+++ scripts.js
@@ -0,0 +1,9 @@
+
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues(){
+    console.log("funcionou")
+}
+
+
+convertButton.addEventListener("click", convertValues)
